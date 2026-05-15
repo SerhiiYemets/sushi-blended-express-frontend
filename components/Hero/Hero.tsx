9f095e4db140cef00d9993b4078135b496b32586
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-05-15 18:46:48 +0200

    update Hero.jmg

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -5,14 +5,14 @@ export default function Hero() {
     return (
         <section className={css.hero}>
             <Image
-                src="https://images.unsplash.com/photo-1553621042-f6e147245754?auto=format&fit=crop&w=1600&q=70"
-                alt=""
+                src="/Hero.jpg"
+                alt="Sushi hero"
                 fill
                 priority
-                fetchPriority="high"
                 sizes="100vw"
                 className={css.heroImage}
             />
+
             <div className={css.heroContent}>
                 <h1 className={css.heroTitle}>Nejlepší sushi v Kolíně</h1>
                 <p className={css.heroSubtitle}>Čerstvé, rychlé, chutné 🍣</p>
